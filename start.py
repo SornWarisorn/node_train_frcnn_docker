2a5dec70c8618e3a710b5d2c4f2f9b4a1cf94132
SornWarisorn <56562334+SornWarisorn@users.noreply.github.com>
2021-02-13 17:10:40 +0700

    docker

--- start.py
+++ start.py
@@ -3,10 +3,10 @@ import sys
 
 os.system('sudo mv protobuf /frcnn_docker/tensorflow1/models/research')
 os.system('sudo mv os_research.py /frcnn_docker/tensorflow1/models/research')
-os.system('sudo mv .bashrc /home/vsa01')
+#os.system('sudo mv .bashrc /home/vsa01')
 
 
-os.chdir('/frcnn_docker/node_train_frcnn_docker/training')
+os.chdir('/node_train_frcnn_docker/training')
 os.system('python3 mv.py')
 
 
