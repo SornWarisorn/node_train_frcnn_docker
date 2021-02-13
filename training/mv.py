d416d6cbec8ef3bfe2b58c4e575cfed53196be6a
SornWarisorn <56562334+SornWarisorn@users.noreply.github.com>
2021-02-13 16:11:04 +0700

    docker

--- training/mv.py
+++ training/mv.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 
-os.system('sudo mv config /home/vsa01/Desktop/tensorflow1/models/research/object_detection/training')
-os.system('sudo mv roslab /home/vsa01/Desktop/tensorflow1/models/research/object_detection/training')
-os.system('sudo mv script /home/vsa01/Desktop/tensorflow1/models/research/object_detection/training')
+os.system('sudo mv config /frcnn_docker/tensorflow1/models/research/object_detection/training')
+os.system('sudo mv roslab /frcnn_docker/tensorflow1/models/research/object_detection/training')
+os.system('sudo mv script /frcnn_docker/tensorflow1/models/research/object_detection/training')
 
 
 
